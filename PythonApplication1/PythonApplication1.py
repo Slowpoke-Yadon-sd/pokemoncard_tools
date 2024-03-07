c228c023a88b0dab84f7d1776cb91f37c3b7511a
daigo <daigo@LAPTOP-TENHHRPH>
2024-03-07 23:01:55 +0900

    供養#

--- PythonApplication1/PythonApplication1.py
+++ PythonApplication1/PythonApplication1.py
@@ -7,12 +7,24 @@ import sys
 from bs4 import BeautifulSoup
 from selenium import webdriver
 import urllib.request
-from io import BytesIO 
+from io import BytesIO
+import csv
 
 PDF_NAME = "デッキプロキシ.pdf"
 BASE_URL = "https://www.pokemon-card.com/"
 DECK_URL = BASE_URL + "deck/result.html/deckID/"
-IMAGE_POSITIONS = [(30, 600), (203, 600), (376, 600), (30, 370), (203, 370), (376, 370), (30, 140), (203, 140), (376, 140)]
+image_width = 6.2*cm
+image_height = 8.7*cm
+#28.3cm 28.6でぴった
+# 画像の配置位置を計算
+IMAGE_POSITIONS = [
+    # 1行目
+    (1*cm, 28*cm - image_height),(1.2*cm+image_width-0.1*cm, 28*cm - image_height),(1.4*cm+2*image_width-0.2*cm, 28*cm - image_height),
+    # 2行目
+    (1*cm, 28*cm - 2*image_height),(1.2*cm+image_width-0.1*cm, 28*cm - 2*image_height),(1.4*cm+2*image_width-0.2*cm, 28*cm - 2*image_height),
+    # 3行目
+    (1*cm, 28*cm - 3*image_height),(1.2*cm+image_width-0.1*cm, 28*cm - 3*image_height),(1.4*cm+2*image_width-0.2*cm, 28*cm - 3*image_height),
+]
 
 def resource_path(relative_path):
     try:
@@ -26,7 +38,7 @@ def fetch_image_url(url):
     return Image.open(BytesIO(buffer))
 
 def get_deck_data(deck_id):
-    driver = webdriver.Chrome(resource_path("C:\chromedriver"))
+    driver = webdriver.Chrome()
     driver.get(DECK_URL + deck_id)
     html = driver.page_source
     soup = BeautifulSoup(html, "html.parser")
@@ -35,9 +47,9 @@ def get_deck_data(deck_id):
     image_urls = [BASE_URL + img.get("src") for img in soup.find(id="cardImagesView").find_all('img')]
     return quantities, image_urls
 
-def generate_pdf(deck_id):
+def generate_pdf(deck_id, deck_name):
     quantities, image_urls = get_deck_data(deck_id)
-    file_path = os.path.expanduser("~") + "/Desktop/" + PDF_NAME
+    file_path = os.path.expanduser("~") +"\\"+ deck_name+".pdf"
     pdf = canvas.Canvas(file_path, pagesize=portrait(A4))
 
     card_index = 0
@@ -48,15 +60,22 @@ def generate_pdf(deck_id):
             if card_index >= len(quantities):
                 break
             img = fetch_image_url(image_urls[card_index])
-            pdf.drawInlineImage(img, pos[0], pos[1], width=6*cm, height=8*cm)
+            pdf.drawInlineImage(img, pos[0], pos[1], width=6.2*cm, height=8.7*cm)
             quantities[card_index] -= 1
         if i < 6:
             pdf.showPage()
 
     pdf.save()
     print("プロキシが生成されました")
+    
+def read_decks_from_csv(csv_file):
+        with open(csv_file, mode='r', encoding='utf-8') as file:
+            reader = csv.reader(file)
+            for row in reader:
+                deck_name, deck_id = row
+                print(f"Generating PDF for {deck_name}...{deck_id}")
+                generate_pdf(deck_id, deck_name)
 
 if __name__ == "__main__":
-    print("デッキコードを入力してください")
-    deck_id = input()
-    generate_pdf(deck_id)
+   csv_file_path =os.path.expanduser("~")+"\\"+'decks.csv'  # CSVファイルのパス
+   read_decks_from_csv(csv_file_path)
